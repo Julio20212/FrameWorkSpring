6442c9372dd7743e657bf67088522ad5a4f9a1cb
julio20212 <162452155+Julio20212@users.noreply.github.com>
2024-10-30 16:39:39 -0300

    Add files via upload

---
+++ main/java/com/example/demo/PrincipalController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PrincipalController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	@GetMapping("/compras")
+	public String compras() {
+		return "compras";
+	}
+	@GetMapping("/vendas")
+	public String vendas() {
+		return "vendas";
+	}
+	@GetMapping("/estoque")
+	public String estoque() {
+		return "estoque";
+	}
+}
